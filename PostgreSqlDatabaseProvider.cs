3aff20d886cbd633cf1f7b6f23175aee3337d543
Karl Harnagy <kharnagy@inedo.com>
2016-09-28 10:54:51 -0400

    Fixed #1

--- PostgreSqlDatabaseProvider.cs
+++ PostgreSqlDatabaseProvider.cs
@@ -33,11 +33,11 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
         public override Task ExecuteQueryAsync(string query, CancellationToken cancellationToken) => this.ExecuteNonQueryAsync(query, cancellationToken);
         public async Task InitializeDatabaseAsync(CancellationToken cancellationToken)
         {
-            var state = await this.GetStateAsync(cancellationToken);
+            var state = await this.GetStateAsync(cancellationToken).ConfigureAwait(false);
             if (state.IsInitialized)
                 return;
 
-            await this.ExecuteNonQueryAsync(Resources.Initialize, cancellationToken);
+            await this.ExecuteNonQueryAsync(Resources.Initialize, cancellationToken).ConfigureAwait(false);
         }
         public Task UpgradeSchemaAsync(IReadOnlyDictionary<int, Guid> canoncialGuids, CancellationToken cancellationToken)
         {
@@ -48,7 +48,7 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
             var tableName = await this.ExecuteScalarAsync(
                 "select table_name from information_schema.tables where table_name='__buildmaster_dbschemachanges'",
                 cancellationToken
-            ) as string;
+            ).ConfigureAwait(false) as string;
 
             if (tableName == null)
                 return new ChangeScriptState(false);
@@ -57,13 +57,13 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
                 "select * from __buildmaster_dbschemachanges",
                 GetRecord,
                 cancellationToken
-            );
+            ).ConfigureAwait(false);
 
             return new ChangeScriptState(1, records);
         }
         public async Task ExecuteChangeScriptAsync(ChangeScriptId scriptId, string scriptName, string scriptText, CancellationToken cancellationToken)
         {
-            var scripts = await this.GetStateAsync(cancellationToken);
+            var scripts = await this.GetStateAsync(cancellationToken).ConfigureAwait(false);
             if (scripts.Scripts.Any(s => scriptId.ScriptId == s.Id.ScriptId))
             {
                 this.LogInformation(scriptName + " already executed. Skipping...");
@@ -73,7 +73,7 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
             bool success;
             try
             {
-                await this.ExecuteNonQueryAsync(scriptText, cancellationToken);
+                await this.ExecuteNonQueryAsync(scriptText, cancellationToken).ConfigureAwait(false);
                 success = true;
                 this.LogInformation(scriptName + " executed successfully.");
             }
@@ -83,7 +83,7 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
                 this.LogError(scriptName + " failed: " + ex.Message);
             }
 
-            await this.RecordResultAsync(scriptId, scriptName, success);
+            await this.RecordResultAsync(scriptId, scriptName, success).ConfigureAwait(false);
         }
 
         protected override void Dispose(bool disposing)
@@ -101,26 +101,26 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
         private async Task<NpgsqlConnection> CreateConnectionAsync()
         {
             var conn = new NpgsqlConnection(this.ConnectionString);
-            await conn.OpenAsync();
+            await conn.OpenAsync().ConfigureAwait(false);
             return conn;
         }
         private async Task ExecuteNonQueryAsync(string query, CancellationToken cancellationToken)
         {
-            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync))
+            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync.ConfigureAwait(false)))
             {
                 cmd.CommandTimeout = 0;
-                await cmd.ExecuteNonQueryAsync(cancellationToken);
+                await cmd.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
             }
         }
         private async Task<List<TRow>> ExecuteDataTableAsync<TRow>(string query, Func<NpgsqlDataReader, TRow> adapter, CancellationToken cancellationToken)
         {
-            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync))
+            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync.ConfigureAwait(false)))
             {
                 cmd.CommandTimeout = 0;
-                using (var reader = (NpgsqlDataReader)await cmd.ExecuteReaderAsync(cancellationToken))
+                using (var reader = (NpgsqlDataReader)await cmd.ExecuteReaderAsync(cancellationToken).ConfigureAwait(false))
                 {
                     var results = new List<TRow>();
-                    while (await reader.ReadAsync(cancellationToken))
+                    while (await reader.ReadAsync(cancellationToken).ConfigureAwait(false))
                     {
                         results.Add(adapter(reader));
                     }
@@ -131,10 +131,10 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
         }
         private async Task<object> ExecuteScalarAsync(string query, CancellationToken cancellationToken)
         {
-            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync))
+            using (var cmd = new NpgsqlCommand(query, await this.connection.ValueAsync.ConfigureAwait(false)))
             {
                 cmd.CommandTimeout = 0;
-                return await cmd.ExecuteScalarAsync(cancellationToken);
+                return await cmd.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
             }
         }
         private static ChangeScriptExecutionRecord GetRecord(NpgsqlDataReader reader)
@@ -142,7 +142,7 @@ namespace Inedo.BuildMasterExtensions.PostgreSql
             return new ChangeScriptExecutionRecord(
                 new ChangeScriptId((int)reader["script_id"], (long)reader["numeric_release_number"]),
                 (string)reader["script_name"],
-                new DateTime(((NpgsqlTypes.NpgsqlDateTime)reader["executed_date"]).ToUniversalTime().Ticks, DateTimeKind.Utc),
+                (DateTime)reader["executed_date"],
                 (YNIndicator)(string)reader["success_indicator"]
             );
         }
