bf01a38ba867eeb9b8d2a511735e713c2bb34675
Ben Lubar <ben.lubar@gmail.com>
2018-05-14 11:52:45 -0500

    Reorganize. Update README.md and LICENSE.

---
+++ PostgreSql/InedoExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PostgreSQL")]
+[assembly: AssemblyDescription("Database integration for PostgreSQL 8.0 or later.")]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
---
+++ PostgreSql/InedoExtension/Resources/Initialize.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE __buildmaster_dbschemachanges (
+  numeric_release_number bigint not null,
+  script_id int not null,
+  script_name varchar(50) not null,
+  executed_date timestamp not null,
+  success_indicator char(1) not null,
+
+  CONSTRAINT __buildmaster_dbschemachangespk
+	PRIMARY KEY (numeric_release_number, script_id)
+)
+;
+
+INSERT INTO __buildmaster_dbschemachanges
+	(numeric_release_number, script_id, script_name, executed_date, success_indicator)
+VALUES
+	(0, 0, 'create table __buildmaster_dbschemachanges', current_timestamp at time zone 'utc', 'Y')
+;
